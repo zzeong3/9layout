0558635e1b5415816eac53e324029ce7604044f1
zzeong3 <zzeong3@gamil.com>
2022-09-27 16:12:31 +0900

    department까지

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//변수 설정
+const btnCall = document.querySelector(".btnCall");
+const menuMo = document.querySelector(".menuMo");
+
+//이벤트 바인딩
+//btnCall을 클릭할때
+btnCall.onclick = function(e){
+    //링크이동금지
+    e.preventDefault();
+
+    //btnCall에 on이 있으면 제거하고, 없으면 추가
+    btnCall.classList.toggle('on');
+    //menuMo에 on이 있으면 제거하고, 없으면 추가
+    menuMo.classList.toggle('on');
+}
